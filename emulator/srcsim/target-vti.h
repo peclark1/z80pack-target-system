0cda58d7a93559981f793006657a0669af22cdfa
peclark1 <37427710+peclark1@users.noreply.github.com>
2026-08-25 03:34:35 -0500

    Correct historical VTI mapping to F800H

--- emulator/srcsim/target-vti.h
+++ emulator/srcsim/target-vti.h
@@ -1,9 +1,7 @@
 #ifndef TARGET_VTI_H
 #define TARGET_VTI_H
 
-#include "simdefs.h"
-
-#define TARGET_VTI_BASE 0x8800
+#define TARGET_VTI_BASE 0xf800
 #define TARGET_VTI_SIZE 0x0400
 #define TARGET_VTI_COLS 64
 #define TARGET_VTI_ROWS 16
@@ -11,8 +9,5 @@
 void target_vti_init(void);
 void target_vti_reset(void);
 void target_vti_exit(void);
-
-BYTE target_vti_keyboard_data_in(void);
-BYTE target_vti_keyboard_status_in(void);
 
 #endif
